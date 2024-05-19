eafd213e91c82d96bd9c60698e9beb2f27dd5cfe
aleksLiss <darklisik.darklisik@yandex.ru>
2024-05-19 11:20:07 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.printf("Размер массива 'ages' равен: %d", ages.length);
+        System.out.printf("Размер массива 'surnames' равен: %d", surnames.length);
+        System.out.printf("Размер массива 'prices' равен : %d", prices.length);
     }
 }
