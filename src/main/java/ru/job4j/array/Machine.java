a1ddd2900f1ccd6345e97628c0299a28e6ebb443
aleksLiss <darklisik.darklisik@yandex.ru>
2024-05-25 14:58:38 +0300

    1. Чтение кода и базовые ошибки [#40205 #497731]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -5,11 +5,8 @@ import java.util.Arrays;
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
-
         int[] result = new int[100];
-
         int size = 0;
-
         for (int coin : coins) {
             while (money - price >= coin) {
                 result[size] = coin;
